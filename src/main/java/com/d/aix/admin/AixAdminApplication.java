cc5c997fdbb862905217b648981188a5edb2ce79
loud <loud@qq.com>
2024-05-24 22:47:32 +0800

    第一次提交

---
+++ src/main/java/com/d/aix/admin/AixAdminApplication.java
@@ -0,0 +1,13 @@
+package com.d.aix.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AixAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AixAdminApplication.class, args);
+    }
+
+}
